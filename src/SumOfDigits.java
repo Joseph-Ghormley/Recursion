a2f27c39239d4e7c500a0f3e9b700501017e49b8
Joseph-Ghormley <54871175+Joseph-Ghormley@users.noreply.github.com>
2023-06-21 22:29:21 -0400

    Add files via upload

--- src/SumOfDigits.java
+++ src/SumOfDigits.java
@@ -1,2 +1,19 @@
-package PACKAGE_NAME;public class SumOfDigits {
+public class SumOfDigits
+{
+    static int SumofDigits(int n)
+    {
+        if (n == 0)
+        {
+            return 0;
+        }
+        return (n % 10 + SumofDigits(n/10));
+    }
+
+    public static void main(String[] args)
+    {
+        int num = 12345;
+        int result = SumofDigits(num);
+        System.out.println("Sum of digits in " +
+                            num + " is " +result);
+    }
 }
